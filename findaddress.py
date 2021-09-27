e876175c9e4b539d60f2c7e37d8daee488fec080
Cody Clark <opuseleven@protonmail.com>
2021-09-27 14:25:05 -0500

    Finished script. Still need to test.

--- findaddress.py
+++ findaddress.py
@@ -3,6 +3,11 @@
 import os
 import sys
 import openpyxl
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 if len(sys.argv) < 2:
     print('Usage: python3 findaddress.py filename.xlsx')
@@ -15,13 +20,22 @@ print(path)
 
 workbook = openpyxl.load_workbook(path)
 
-namecol = null
-citycol = null
-addresscol = null
+driver_options = Options()
+driver_options.headless = True
+driver = webdriver.Firefox(options=driver_options)
+
+def search(term):
+    driver.get("https://www.google.com/search?q=%s" % term)
+    time.sleep(2)
+    address = driver.find_element(By.CLASS_NAME, "LrzXr")
+    return address.text
 
 for sheet in workbook.worksheets:
     titlerow = sheet[1]
     state = sheet.title
+    namecol = null
+    citycol = null
+    addresscol = null
     for cell in titlerow:
         if cell.value = 'Name':
             namecol = cell.column
@@ -29,11 +43,26 @@ for sheet in workbook.worksheets:
             citycol = cell.column
         if cell.value = 'Address':
             addresscol = cell.column
-        else:
-            sheet.insert_cols(5)
-            addresscol = sheet['E']
-
     if namecol = null:
         break
-    for row in sheet.iter_rows():
+    if addresscol = null:
+        sheet.insert_cols(5)
+        addresscol = sheet['E']
+        sheet['E1'] = "Address"
+    for row in sheet.iter_rows(min_row=2):
         # search for address
+        name = row[namecol].value
+        city = row[citycol].value
+        searchterm = Str("%s %s %s"%(name, city, state))
+        address = search(searchterm)
+        row[addresscol] = address
+
+def tearDown(self):
+    self.quit()
+
+tearDown(driver)
+
+# Write workbook
+split_filename = os.path.splitext(filename)
+new_filename = split_filename[0] + '-address' + split_filename[1]
+workbook.save(new_filename)
